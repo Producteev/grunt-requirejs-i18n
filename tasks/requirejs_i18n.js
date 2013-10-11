b22dc54c4e9e6a39227924f17af98a51d55695f8
Aric Lasry <lasry.aric@gmail.com>
2013-10-11 00:14:53 +0000

    Adding grunt generated files

---
+++ tasks/requirejs_i18n.js
@@ -0,0 +1,50 @@
+/*
+ * grunt-requirejs-i18n
+ * https://github.com/Producteev/grunt-requirejs-i18n
+ *
+ * Copyright (c) 2013 Aric Lasry
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('requirejs_i18n', 'Prepend your i18n files from a specific lang (or more) to your modules built with grunt-contrib-requirejs', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" created.');
+    });
+  });
+
+};
